7216f41ced8e78381ef22f5b97f2d979621906c5
Ben Li <ben10723021@gmail.com>
2024-04-09 12:41:44 +0800

    feat: render list result

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,35 +1,43 @@
+<template>
+  <template v-for="introItem in vueMenuIntro">
+    <WelcomeItem>
+      <template #heading>{{ introItem.heading }}</template>
+      {{ introItem.description }}
+    </WelcomeItem>
+  </template>
+</template>
 <script setup lang="ts">
 import WelcomeItem from './WelcomeItem.vue';
+
+interface IMenuIntro {
+  heading: string;
+  description: string;
+}
+// 也可以寫成 const vueMenuIntro: Array<IMenuIntro>
+const vueMenuIntro: IMenuIntro[] = [
+  {
+    heading: 'Documentation',
+    description: 'Vue’s official documentation provides you with all information you need to get started.',
+  },
+  {
+    heading: 'Tooling',
+    description:
+      'This project is served and bundled with Vite. The recommended IDE setup is VSCode + Volar. If you need to test your components and web pages, check out Cypress and Cypress Component Testing. More instructions are available in README.md.',
+  },
+  {
+    heading: 'Ecosystem',
+    description:
+      'Get official tools and libraries for your project: Pinia, Vue Router, Vue Test Utils, and Vue Dev Tools. If you need more resources, we suggest paying Awesome Vue a visit.',
+  },
+  {
+    heading: 'Community',
+    description:
+      'Got stuck? Ask your question on Vue Land, our official Discord server, or StackOverflow. You should also subscribe to our mailing list and follow the official @vuejs twitter account for latest news in the Vue world.',
+  },
+  {
+    heading: 'Support Vue',
+    description:
+      'As an independent project, Vue relies on community backing for its sustainability. You can help us by becoming a sponsor.',
+  },
+];
 </script>
-
-<template>
-  <WelcomeItem>
-    <template #heading>Documentation</template>
-    Vue’s official documentation provides you with all information you need to get started.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #heading>Tooling</template>
-    This project is served and bundled with Vite. The recommended IDE setup is VSCode + Volar. If you need to test your
-    components and web pages, check out Cypress and Cypress Component Testing. More instructions are available in
-    README.md.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #heading>Ecosystem</template>
-    Get official tools and libraries for your project: Pinia, Vue Router, Vue Test Utils, and Vue Dev Tools. If you need
-    more resources, we suggest paying Awesome Vue a visit.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #heading>Community</template>
-    Got stuck? Ask your question on Vue Land, our official Discord server, or StackOverflow. You should also subscribe
-    to our mailing list and follow the official @vuejs twitter account for latest news in the Vue world.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #heading>Support Vue</template>
-    As an independent project, Vue relies on community backing for its sustainability. You can help us by becoming a
-    sponsor.
-  </WelcomeItem>
-</template>
