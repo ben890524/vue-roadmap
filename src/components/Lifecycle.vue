d313c4b5081adf02a04274b7952506cb61200388
Ben Li <ben10723021@gmail.com>
2024-04-10 10:25:44 +0800

    feat: adjust example

---
+++ src/components/Lifecycle.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h1>Lifecycle Counter: {{ counter }}</h1>
+    <button @click="counter += 1">Add <- Click to add counter</button>
+  </div>
+</template>
+<script setup lang="ts">
+import { onBeforeMount, onMounted, onBeforeUnmount, onUnmounted, ref, onBeforeUpdate, onUpdated } from 'vue';
+
+const counter = ref<number>(0);
+
+onBeforeMount(() => {
+  console.log('In onBeforeMount');
+});
+
+onMounted(() => {
+  console.log('In onMounted');
+});
+
+onBeforeUpdate(() => {
+  console.log('In onBeforeUpdate');
+});
+onUpdated(() => {
+  console.log(`In onUpdated and counter is ${counter.value}`);
+});
+
+onBeforeUnmount(() => {
+  console.log('In onBeforeUnmount');
+});
+
+onUnmounted(() => {
+  console.log('In onUnmounted');
+});
+</script>
+
+<script lang="ts">
+export default {
+  name: '',
+};
+</script>
+
+<style scoped lang="scss"></style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,10 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <Lifecycle />
   </div>
 </template>
+<script setup lang="ts">
+import Lifecycle from '@/components/Lifecycle.vue';
+</script>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<style></style>
