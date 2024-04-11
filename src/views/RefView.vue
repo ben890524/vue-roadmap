6c738c04bd62edbd8ac85699d1b5a8df11c26ef9
Ben Li <ben10723021@gmail.com>
2024-04-11 14:17:57 +0800

    feat: ref example

---
+++ src/views/RefView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div>This is count: {{ counter }}</div>
+    <button class="px-2.5 my-2.5 py-1 rounded border" @click="counter += 1">Add count</button>
+    <div>This is ref count: {{ refCounter }}</div>
+    <button class="px-2.5 my-2.5 py-1 rounded border" @click="refCounter += 1">Add ref count</button>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+let counter = 0;
+
+const refCounter = ref(0);
+</script>
+
+<script lang="ts">
+export default {
+  name: '',
+};
+</script>
+
+<style scoped lang="scss"></style>
